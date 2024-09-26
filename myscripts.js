f986aebaa0e1a589d1293504cec54a4dd2835a39
craftycheshire <mini_vann@ymail.com>
2024-09-25 22:34:34 -0500

    splash-page update,, unsuccesful button click

--- myscripts.js
+++ myscripts.js
@@ -4,3 +4,11 @@ element.addEventListener("click", myFunction);
 function myFunction() {
     document.getElementById("demo").innerHTML = "Chuck Norris is cooler than your grandma.";
 }
+
+localStorage.setItem('firstName', 'Obaseki Nosa');
+localStorage.setItem("lastName", "");
+localStorage.setItem("email", "");
+localStorage.getItem("firstName");
+localStorage.getItem("lastName");
+localStorage.getItem("email");
+document.getElementById("subscribe").innerHTML = localStorage.getItem("firstName");
